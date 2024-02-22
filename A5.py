a131cdf79faf84c60bf4015eae5081d775c4c7c0
DavidMaster001 <jonathand.pintob@ecci.edu.co>
2024-02-21 23:57:28 -0500

    Update A5.py

--- A5.py
+++ A5.py
@@ -4,32 +4,24 @@
 import numpy as np
 import math
 
-# Función para la rotación en el eje X
-def rotacion_x(angulo):
-    radianes = math.radians(angulo)
-    return np.array([[1, 0, 0],
-                     [0, math.cos(radianes), -math.sin(radianes)],
-                     [0, math.sin(radianes), math.cos(radianes)]])
-
-# Función para la rotación en el eje Y
-def rotacion_y(angulo):
-    radianes = math.radians(angulo)
-    return np.array([[math.cos(radianes), 0, math.sin(radianes)],
-                     [0, 1, 0],
-                     [-math.sin(radianes), 0, math.cos(radianes)]])
-
-# Función para la rotación en el eje Z
-def rotacion_z(angulo):
-    radianes = math.radians(angulo)
-    return np.array([[math.cos(radianes), -math.sin(radianes), 0],
-                     [math.sin(radianes), math.cos(radianes), 0],
-                     [0, 0, 1]])
-
-# Ejemplo de uso
+# Ángulo en grados
 angulo = 45
-rotacion_x_matriz = rotacion_x(angulo)
-rotacion_y_matriz = rotacion_y(angulo)
-rotacion_z_matriz = rotacion_z(angulo)
+radianes = math.radians(angulo)
+
+# Matriz de rotación en el eje X
+rotacion_x_matriz = np.array([[1, 0, 0],
+                              [0, math.cos(radianes), -math.sin(radianes)],
+                              [0, math.sin(radianes), math.cos(radianes)]])
+
+# Matriz de rotación en el eje Y
+rotacion_y_matriz = np.array([[math.cos(radianes), 0, math.sin(radianes)],
+                              [0, 1, 0],
+                              [-math.sin(radianes), 0, math.cos(radianes)]])
+
+# Matriz de rotación en el eje Z
+rotacion_z_matriz = np.array([[math.cos(radianes), -math.sin(radianes), 0],
+                              [math.sin(radianes), math.cos(radianes), 0],
+                              [0, 0, 1]])
 
 print("Matriz de rotación en el eje X:")
 print(rotacion_x_matriz)
@@ -38,4 +30,4 @@ print("Matriz de rotación en el eje Y:")
 print(rotacion_y_matriz)
 
 print("Matriz de rotación en el eje Z:")
-print(rotacion_z_matriz)
+print(rotacion_z_matriz)   
