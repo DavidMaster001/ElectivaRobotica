7f28b85172ea632e14e8cd2c8445633185e42fc4
DavidMaster001 <jonathand.pintob@ecci.edu.co>
2024-02-22 00:24:54 -0500

    :dolphin:

--- C4.py
+++ C4.py
@@ -1,24 +1,26 @@
+# Importar las librerías necesarias
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# Definir una función para dibujar un vector en 3D
 def dibujar_vector(x, y, z):
+    # Crear una figura y un eje 3D
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111, projection='3d') #Se utiliza para crear un espacio de dibujo tridimensional (3D)
 
-    # Dibuja el sistema de coordenadas
-    ax.quiver(0, 0, 0, 1, 0, 0, color='r', label='X')
-    ax.quiver(0, 0, 0, 0, 1, 0, color='g', label='Y')
-    ax.quiver(0, 0, 0, 0, 0, 1, color='b', label='Z')
+    # Dibujar el sistema de coordenadas en rojo (X), verde (Y) y azul (Z)
+    ax.quiver(0, 0, 0, 1, 0, 0, color='r', label='X') #primer conjunto de 0 es para indicar el origen 
+    ax.quiver(0, 0, 0, 0, 1, 0, color='g', label='Y') #segundo conjuto de numeros corresponde a (x,y,z)
+    ax.quiver(0, 0, 0, 0, 0, 1, color='b', label='Z') 
 
-    # Dibuja el vector ingresado por el usuario
-    ax.quiver(0, 0, 0, x, y, z, color='black', label='Vector')
+    # Dibujar el vector ingresado por el usuario en negro
+    ax.quiver(0, 0, 0, x, y, z, color='black', label='Vector') # quiver trazar un vector
 
+    # Etiquetas de los ejes y título del gráfico
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    ax.set_title('Sistema de Coordenadas 3D')
-
-    ax.legend()
+    ax.set_title('Sistema de Coordenadas 3D')   
     plt.show()
 
 # Ingreso de coordenadas del vector por parte del usuario
@@ -26,5 +28,6 @@ x = float(input("Ingrese la coordenada X del vector: "))
 y = float(input("Ingrese la coordenada Y del vector: "))
 z = float(input("Ingrese la coordenada Z del vector: "))
 
-# Dibujar el vector
+# Dibujar el vector utilizando la función definida anteriormente
 dibujar_vector(x, y, z)
+
--- prueba.py
+++ prueba.py
@@ -1,33 +1,45 @@
 import matplotlib.pyplot as plt
 
-# Crear una figura y un eje
+# Create a new figure
 fig, ax = plt.subplots()
 
-# Dibujar las letras del nombre "DAVID" en mayúsculas
-# Letra D
-ax.plot([0, 0], [0, 1], 'b-')
-ax.plot([0, 0.5], [1, 0.5], 'b-')
-ax.plot([0.5, 1], [0.5, 1], 'b-')
-ax.plot([1, 1], [1, 0], 'b-')
+# Set the size of the figure
+fig.set_size_inches(5, 2)
 
-# Letra A
-ax.plot([1.2, 1.4], [0, 1], 'b-')
-ax.plot([1.4, 1.6], [1, 0], 'b-')
-ax.plot([1.3, 1.5], [0.5, 0.5], 'b-')
+# Remove the spines (i.e., the lines around the edges) of the plot
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_position('zero')
+ax.spines['left'].set_position('zero')
 
-# Letra V
-ax.plot([1.7, 1.8], [0, 1], 'b-')
-ax.plot([1.8, 1.9], [1, 0], 'b-')
+# Set the x and y limits of the plot
+ax.set_xlim(-1, 9)
+ax.set_ylim(-2, 5)
 
-# Letra I
-ax.plot([2, 2], [0, 1], 'b-')
+# Draw the letters of the name "DAVID"
+ax.text(1, 1, "D", ha='center', va='center')
+ax.text(2, 1, "A", ha='center', va='center')
+ax.text(3, 1, "V", ha='center', va='center')
+ax.text(4, 1, "I", ha='center', va='center')
+ax.text(5, 1, "D", ha='center', va='center')
 
-# Letra D
-ax.plot([2.2, 2.2], [0, 1], 'b-')
-ax.plot([2.2, 2.4], [1, 0.5], 'b-')
-ax.plot([2.4, 2.2], [0.5, 0], 'b-')
+ax.text(1, 2, "D", ha='center', va='center')
+ax.text(2, 2, "I", ha='center', va='center')
+ax.text(3, 2, "L", ha='center', va='center')
+ax.text(4, 2, "A", ha='center', va='center')
+ax.text(5, 2, "N", ha='center', va='center')
 
-# Configurar ejes y mostrar la figura
-ax.axis('equal')
-ax.axis('off')
+ax.text(1, 3, "A", ha='center', va='center')
+ax.text(2, 3, "D", ha='center', va='center')
+ax.text(3, 3, "R", ha='center', va='center')
+ax.text(4, 3, "I", ha='center', va='center')
+ax.text(5, 3, "A", ha='center', va='center')
+ax.text(6, 3, "N", ha='center', va='center')
+ax.text(7, 3, "A", ha='center', va='center')
+
+# Remove the tick marks from the plot
+ax.set_xticks([])
+ax.set_yticks([])
+
+# Show the plot
 plt.show()
