67d8b244b55abf5bd12e1cd516e7f73b78d3cf61
DavidMaster001 <jonathand.pintob@ecci.edu.co>
2024-02-21 22:55:04 -0500

    actualizado
    :D

--- A4.py
+++ A4.py
@@ -8,12 +8,7 @@ a = 3.9083e-3
 b = -5.775e-7
 R0 = 100  # Resistencia nominal a 0°C
 
-# Función para calcular la resistencia en función de la temperatura
-def cal_res(temperatura):
-    R = R0 * (1 + a*temperatura + b*temperatura**2)
-    return R
-
 # Ejemplo de uso
 temperatura = 100  # Temperatura en grados Celsius
-resistencia = cal_res(temperatura)
+resistencia = R0 * (1 + a*temperatura + b*temperatura**2)  # Función para calcular la resistencia en función de la temperatura
 print(f"Para una temperatura de {temperatura}°C, la resistencia es de {resistencia} ohmios")
