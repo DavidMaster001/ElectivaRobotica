136829eb48b39ddb7ef3d87ebfe91258f8977764
DyllamArevalo <dyllam.arevalov@ecci.edu.co>
2024-02-29 17:28:42 -0500

    LAST

--- A4.py
+++ A4.py
@@ -4,11 +4,11 @@
 import math
 
 # Coeficientes de la ecuación de Callendar-Van Dusen para una RTD de platino (PT100)
-a = 3.9083e-3
-b = -5.775e-7
-R0 = 100  # Resistencia nominal a 0°C
+R0 = 100  
+A = 3.9083e-3
+B = -5.775e-7
+C = -4.183e-12
+TEMP = 25
 
-# Ejemplo de uso
-temperatura = 100  # Temperatura en grados Celsius
-resistencia = R0 * (1 + a*temperatura + b*temperatura**2)  # Función para calcular la resistencia en función de la temperatura
-print(f"Para una temperatura de {temperatura}°C, la resistencia es de {resistencia} ohmios")
+R = R0 * (1 + A * TEMP + B * TEMP*2 + C * (TEMP - 100) * TEMP*3)  # Función para calcular la resistencia en función de la temperatura
+print(f"\nLa resistencia a {TEMP} grados ° es de: {R:.2f} Ω \n")
--- A5.py
+++ A5.py
@@ -4,30 +4,39 @@
 import numpy as np
 import math
 
-# Ángulo en grados
+def rotacion_x(angulo):
+    radianes = math.radians(angulo)
+    matriz = np.array([[1, 0, 0],
+                       [0, math.cos(radianes), -math.sin(radianes)],
+                       [0, math.sin(radianes), math.cos(radianes)]])
+    return matriz
+
+def rotacion_y(angulo):
+    radianes = math.radians(angulo)
+    matriz = np.array([[math.cos(radianes), 0, math.sin(radianes)],
+                       [0, 1, 0],
+                       [-math.sin(radianes), 0, math.cos(radianes)]])
+    return matriz
+
+def rotacion_z(angulo):
+    radianes = math.radians(angulo)
+    matriz = np.array([[math.cos(radianes), -math.sin(radianes), 0],
+                       [math.sin(radianes), math.cos(radianes), 0],
+                       [0, 0, 1]])
+    return matriz
+
+# Ejemplo de uso:
 angulo = 45
-radianes = math.radians(angulo)
 
-# Matriz de rotación en el eje X
-rotacion_x_matriz = np.array([[1, 0, 0],
-                              [0, math.cos(radianes), -math.sin(radianes)],
-                              [0, math.sin(radianes), math.cos(radianes)]])
-
-# Matriz de rotación en el eje Y
-rotacion_y_matriz = np.array([[math.cos(radianes), 0, math.sin(radianes)],
-                              [0, 1, 0],
-                              [-math.sin(radianes), 0, math.cos(radianes)]])
-
-# Matriz de rotación en el eje Z
-rotacion_z_matriz = np.array([[math.cos(radianes), -math.sin(radianes), 0],
-                              [math.sin(radianes), math.cos(radianes), 0],
-                              [0, 0, 1]])
+matriz_rotacion_x = rotacion_x(angulo)
+matriz_rotacion_y = rotacion_y(angulo)
+matriz_rotacion_z = rotacion_z(angulo)
 
 print("Matriz de rotación en el eje X:")
-print(rotacion_x_matriz)
+print(matriz_rotacion_x)
 
 print("Matriz de rotación en el eje Y:")
-print(rotacion_y_matriz)
+print(matriz_rotacion_y)
 
 print("Matriz de rotación en el eje Z:")
-print(rotacion_z_matriz)   
+print(matriz_rotacion_z)
