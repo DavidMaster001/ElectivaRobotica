8be6afb89d8105fe1969968f862c4c46c0e30e0c
DavidMaster001 <jonathand.pintob@ecci.edu.co>
2024-02-29 23:17:53 -0500

    c6 ok

--- C6.py
+++ C6.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 # Leer las imágenes en escala de grises
 Renault = cv2.imread('logo1.png', cv2.IMREAD_GRAYSCALE)
-audi = cv2.imread('logo2.png', cv2.IMREAD_GRAYSCALE)
+hyundai = cv2.imread('logo2.png', cv2.IMREAD_GRAYSCALE)
 
 # Aplicar umbralización para obtener imágenes binarias
 _, thres_Renault = cv2.threshold(Renault, 240, 255, cv2.THRESH_BINARY)
-_, thres_audi = cv2.threshold(audi, 240, 255, cv2.THRESH_BINARY)# Encontrar los contornos en las imágenes binarias
+_, thres_hyundai = cv2.threshold(hyundai, 240, 255, cv2.THRESH_BINARY)# Encontrar los contornos en las imágenes binarias
 
 contornos_Renault, _ = cv2.findContours(thres_Renault, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contornos_audi, _ = cv2.findContours(thres_audi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contornos_hyundai, _ = cv2.findContours(thres_hyundai, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Crear listas para almacenar las coordenadas de los puntos de los contornos
 coord_x_Renault = []
@@ -22,13 +22,13 @@ for contorno in contornos_Renault:
         coord_x_Renault.append(x)
         coord_y_Renault.append(y)
 
-coord_x_audi = []
-coord_y_audi = []
-for contorno in contornos_audi:
+coord_x_hyundai = []
+coord_y_hyundai = []
+for contorno in contornos_hyundai:
     for punto in contorno:
         x, y = punto[0]
-        coord_x_audi.append(x)
-        coord_y_audi.append(y)
+        coord_x_hyundai.append(x)
+        coord_y_hyundai.append(y)
 
 # Crear un gráfico de dispersión para visualizar los contornos
 plt.figure(figsize=(12, 6))
@@ -39,8 +39,8 @@ plt.xlabel('Coordenada X')
 plt.ylabel('Coordenada Y')
 
 plt.subplot(1, 2, 2)
-plt.scatter(coord_x_audi, coord_y_audi, s=1, color='b')
-plt.title('Contorno del logo de Audi')
+plt.scatter(coord_x_hyundai, coord_y_hyundai, s=1, color='b')
+plt.title('Contorno del logo de hyundai')
 plt.xlabel('Coordenada X')
 plt.ylabel('Coordenada Y')
 
